b5438fc4e7c0f36ddab1288781e8d47dc195728c
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-06-11 17:34:57 -0500

    Create StudentController, Student class and Student-form
    Implemented data binding using Spring Form Tags and Data Binding.

---
+++ src/com/nadeem/springdemo/mvc/StudentController.java
@@ -0,0 +1,25 @@
+package com.nadeem.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+	@RequestMapping("/showForm")
+	public String showForm(Model theModel){
+		
+		// create the student object
+		Student theStudent = new Student();
+		
+		// add student to model attribute. This model will be passed
+		// on to the form where the Spring Form with it's special tags
+		// will inject the values in to the student object that was in the
+		// model attribute.
+		theModel.addAttribute("student", theStudent);
+		
+		return "student-form";
+	}
+}
