6c312fa7646e1812b96e5da1ce5243db6c439b5b
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-06-11 17:11:50 -0500

    Implemented RequestMapping

--- WebContent/WEB-INF/view/helloworld.jsp
+++ WebContent/WEB-INF/view/helloworld.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,8 +7,9 @@
 <title>Hello World Processed Form</title>
 </head>
 <body>
-<h2>Hello World of Spring!</h2>
-<br></br>
-Student Name: ${param.studentName}
+	<h2>Hello World of Spring!</h2>
+	<br></br> Student Name: ${param.studentName}
+	<br></br>
+	The message: ${message}
 </body>
 </html>
--- src/com/nadeem/springdemo/mvc/HelloWorldController.java
+++ src/com/nadeem/springdemo/mvc/HelloWorldController.java
@@ -1,9 +1,14 @@
 package com.nadeem.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 	
 	// need a controller method to show initial HTML form
@@ -15,6 +20,42 @@ public class HelloWorldController {
 	// need a controller method to process the form
 	@RequestMapping("/processForm")
 	public String processForm(){
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model){
+		
+		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+		
+		// convert the data to all upper case
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Yo! "+theName;
+		
+		// adding the message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+		
+		// read the request parameter from the HTML form
+//		String theName = request.getParameter("studentName");
+		
+		// convert the data to all upper case
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Hey! My friend from v3 "+theName;
+		
+		// adding the message to the model
+		model.addAttribute("message", result);
+		
 		return "helloworld";
 	}
 }
