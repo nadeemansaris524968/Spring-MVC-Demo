17dc601f49c8b315e0adfb99a55eab53c098df9d
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-06-12 12:47:07 -0500

    Add Valid annotation to Model object to perform validation based on Model class
    Results of the validation will be stored in BindingResult object

--- src/com/nadeem/springdemo/mvc/CustomerController.java
+++ src/com/nadeem/springdemo/mvc/CustomerController.java
@@ -1,7 +1,11 @@
 package com.nadeem.springdemo.mvc;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,8 +21,14 @@ public class CustomerController {
 	}
 	
 	@RequestMapping("/processForm")
-	public String processForm(){
+	public String processForm(
+			@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult){
 		
-		return "customer-confirmation";
+		if (theBindingResult.hasErrors()){
+			return "customer-form";
+		}
+		else {
+			return "customer-confirmation";
+		}
 	}
 }
